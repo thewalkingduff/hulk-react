f97cb7c9686d0823720fab130ba1eec03ff03f85
thewalkingduff <62387646+thewalkingduff@users.noreply.github.com>
2021-06-30 01:27:51 -0400

    remove old api url

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ function App() {
 
   useEffect(() => {
     async function getData() {
-      const api = 'https://gateway.marvel.com/v1/public/characters?name=hulk&ts=1&apikey=d832b399dbb6f81d7a7a1c331467d90b&hash=8194cabb24e7c3f11f54b8dca39c955a'
+      const api = 'https://gateway.marvel.com:443/v1/public/comics?dateRange=2010-01-01%2C2021-12-31&characters=1009351&limit=99&ts=1&apikey=d832b399dbb6f81d7a7a1c331467d90b&hash=8194cabb24e7c3f11f54b8dca39c955a'
       const result = await fetch(api)
       const getResult = await result.json()
-      setData(getResult.data.results[0])
+      // setData(getResult.data.results[0])
+      setData(getResult.data.results)
       setLoaded(true)
     }
     getData()
@@ -22,7 +23,7 @@ function App() {
   return (<>
     <h1>HULK COMIC BOOKS!</h1>
 
-    <div key={data.id} className='container-comics'>
+    {/* <div key={data.id} className='container-comics'>
       {data && data.comics && data.comics.items.map((comic, i) =>
         <div key={i} className="comic-books">
           <a href={data.urls[0].url} target="_blank">
@@ -30,6 +31,16 @@ function App() {
           </a>
           <h3>{comic.name}</h3>
           <p>{data.description}</p>
+        </div>
+      )}
+    </div> */}
+    <div key={data.id} className='container-comics'>
+      {data && data.map((comic, i) =>
+        <div key={i} className="comic-books">
+          <a href={comic.urls[0].url} target="_blank">
+            <img src={`${comic.thumbnail.path}/portrait_fantastic.${comic.thumbnail.extension}`} />
+          </a>
+          <h3>{comic.title}</h3>
         </div>
       )}
     </div>
